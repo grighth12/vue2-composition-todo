84d2fba5838742f3d6c8367297a4ef052d51a521
grighth12 <grighth12@gmail.com>
2024-03-02 16:18:01 +0900

    feat: pinia store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,40 @@
-<script setup lang="ts">
-import { ref } from "vue";
+<script lang="ts">
+import { useCounterStore } from "@/stores/counter";
+import { storeToRefs } from "pinia";
+import { defineComponent, ref } from "vue";
 
-const count = ref(0);
+export default defineComponent({
+  name: "HomeView",
+  setup() {
+    const count = ref(0);
 
-const handleAdd = () => {
-  console.log("dfsfsd");
-  count.value++;
-};
+    const counterStore = useCounterStore();
+    const { count: pCount, increment } = storeToRefs(counterStore);
 
-const handleMinus = () => {
-  console.log("minus");
-  count.value--;
-};
+    const handleAdd = () => {
+      console.log("dfsfsd");
+      count.value++;
+    };
+
+    const handleMinus = () => {
+      console.log("minus");
+      count.value--;
+    };
+
+    const handleReset = () => {
+      counterStore.$patch({ count: 0 });
+    };
+
+    return {
+      count,
+      pCount,
+      increment,
+      handleAdd,
+      handleMinus,
+      handleReset,
+    };
+  },
+});
 </script>
 
 <template>
@@ -22,5 +45,10 @@ const handleMinus = () => {
       <button @click="handleAdd">+</button>
       <button @click="handleMinus">-</button>
     </div>
+
+    <h2>Pinia Counter</h2>
+    <span>{{ pCount }}</span>
+    <button @click="increment">+</button>
+    <button @click="handleReset">reset</button>
   </main>
 </template>
